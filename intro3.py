21e01cd32e5c3e10d8392fc036d1de24376a486a
Daphne Watson <daphne.renee.laurels@gmail.com>
2020-07-11 14:05:57 -0700

    note taking on classes and objects

--- intro3.py
+++ intro3.py
@@ -1,6 +1,9 @@
 # Glossary
 
 # Functions https://www.learnpython.org/en/Functions
+# Classes and Objects https://www.learnpython.org/en/Classes_and_Objects
+    # Accessing Object Variables
+    # Accessing Object Functions
 
     # What are Functions? 
         # Functions are a convenient way to divide your code 
@@ -41,6 +44,121 @@ my_function_with_args("Daphne", "well")
 def sum_two_numbers(a, b):
     return a + b
 sum_two_numbers(5, 6)
+
+# How do you call functions in Python? 
+    # Simply write the function's name followed by (), 
+    # placing any required arguments within the brackets. 
+    # For example, lets call the functions written above 
+    # (in the previous example):
+
+# Define our 3 functions
+def my_function():
+    print("Hello From My Function!")
+
+def my_function_with_args(username, greeting):
+    print("Hello, %s , From My Function!, I wish you %s"%(username, greeting))
+
+def sum_two_numbers(a, b):
+    return a + b
+
+    # print(a simple greeting)
+my_function()
+
+    #prints - "Hello, John Doe, From My Function!, I wish you a great year!"
+my_function_with_args("John Doe", "a great year!")
+
+    # after this line x will hold the value 3!
+x = sum_two_numbers(1,2)
+
+# Classes and Objects
+    # Objects are an encapsulation of variables and functions into 
+    # a single entity. Objects get their variables and functions 
+    # from classes. Classes are essentially a template to create 
+    # your objects.
+
+    # A very basic class would look something like this:
+    class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+    # We'll explain why you have to include that "self" as a parameter 
+    # a little bit later. First, to assign the above class(template) 
+    # to an object you would do the following:
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+
+    # Now the variable "myobjectx" holds an object of the class "MyClass" 
+    # that contains the variable and the function defined within the class
+    #  called "MyClass".
+
+# Accessing Object Variables
+    # To access the variable inside of the newly created object "myobjectx" 
+    # you would do the following:
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+
+myobjectx.variable
+
+    # So for instance the below would output the string "blah":
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+
+print(myobjectx.variable)
+
+    # You can create multiple different objects that are of the same class
+    # (have the same variables and functions defined). 
+    # However, each object contains independent copies of the variables 
+    # defined in the class. For instance, if we were to define another 
+    # object with the "MyClass" class and then change the string in 
+    # the variable above:
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+myobjecty = MyClass()
+
+myobjecty.variable = "yackity"
+
+# Then print out both values
+print(myobjectx.variable) # blah
+print(myobjecty.variable) # yackity
+
+# Accessing Object Functions
+    # To access a function inside of an object you use notation 
+    # similar to accessing a variable:
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+
+myobjectx.function()
+
+    # The above would print out the message, 
+    # "This is a message inside the class."
+
 
 
 
